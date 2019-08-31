eaf39f9eaaee1b0d45ba7187331fd47438eb0a7a
Andrew <a.john.jacobsen@gmail.com>
2019-08-31 12:29:52 -0600

    add TDC adagrad

---
+++ src/agents/TDCadagrad.py
@@ -0,0 +1,6 @@
+import numpy as np
+from src.agents.TDC import TDV
+from src.agents.AdaGrad import AdaGrad
+
+class TDCadagrad(TDC, AdaGrad):
+    pass
--- src/agents/agents.py
+++ src/agents/agents.py
@@ -2,6 +2,7 @@ from .BaseTD import BaseTD
 from .TDC import TDC
 from .GTD2 import GTD2
 from .GTD2adagrad import GTD2adagrad
+from .TDCadagrad import TDCadagrad
 
 def getAgent(name):
     if name == 'TDC':
@@ -12,6 +13,9 @@ def getAgent(name):
 
     elif name == 'GTD2adagrad':
         return GTD2adagrad
+
+    elif name == 'TDCadagrad':
+        return TDCadagrad
 
     elif name == 'TD':
         return BaseTD
