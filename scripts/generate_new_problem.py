1b85c2a29856c099dfdf758f5b0b4dae1a75c6c7
andy <andnpatterson@gmail.com>
2019-09-13 14:33:45 -0600

    add a bunch of stepsize experiments

---
+++ scripts/generate_new_problem.py
@@ -0,0 +1,24 @@
+import sys
+import json
+import glob
+import os
+sys.path.append(os.getcwd())
+
+from src.utils.path import fileName, up
+
+prob_folder = sys.argv[1]
+new_prob = sys.argv[2]
+jsons = glob.glob(f'{prob_folder}/**/*.json')
+
+old_prob = fileName(prob_folder)
+
+for json in jsons:
+    with open(json, 'r') as f:
+        d = f.read()
+
+        new = d.replace(old_prob, new_prob)
+        new_path = json.replace(old_prob, new_prob)
+
+        os.makedirs(up(new_path), exist_ok=True)
+        with open(new_path, 'w') as f:
+            f.write(new)
